ff23e1217d4e930fcfe330ff8d6c3cb41ed5d9e4
Guilherme Terra <by.terra@hotmail.com>
2023-02-03 09:55:27 +0100

    adding migration as forgot to add it to git commit on last push

---
+++ Migrations/20230203083818_RemovingTestEntity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace s21340_exam.Migrations
+{
+    public partial class RemovingTestEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EfTestEntities");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EfTestEntities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EfTestEntities", x => x.Id);
+                });
+        }
+    }
+}
